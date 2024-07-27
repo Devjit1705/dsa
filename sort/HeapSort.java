9774a8bd453c6b5dfbaeebc2cc3ed075d3bddfaf
Devjit1705 <chakraborty.devjit@gmail.com>
2024-07-27 16:09:24 +0530

    Modified the logic for 0 based array

--- sort/HeapSort.java
+++ sort/HeapSort.java
@@ -2,37 +2,44 @@ package dev.sort;
 
 import java.util.Arrays;
 
+/*
+ * @Author Devjit Chakraborty
+ * Heapsort on 0 based array
+ * Time Complexity : O(N log N), here N is number of elements in array.
+ * Auxiliary Space : O(1), since no extra space used.
+ */
 public class HeapSort {
     public static void main (String[] args) {
         int[] arr = {2, 9, 1, 6, 5, 5, 7, 8};
-        buildMaxHeap(arr);
-        for(int i=arr.length-1; i>=0; i--) {
-            Swap.swap(arr, 0, arr.length-1);
-            heapify(arr, 0);
+        int n = arr.length;
+        //Build max heap
+        buildMaxHeap(arr, n);
+        for(int i = n-1; i >= 0; i--) {
+            //swap the first element with ith element
+            Swap.swap(arr, 0, i);
+            heapify(arr, i, 0);
         }
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void buildMaxHeap(int[] array) {
-        for(int i=array.length/2-1; i>=0; i--) {
-            heapify(array, i);
+    private static void buildMaxHeap(int[] arr, int n) {
+        for(int i = n/2-1; i >= 0; i--) {
+            heapify(arr, n, i);
         }
     }
-    private static void heapify(int[] array, int i) {
+    private static void heapify(int[] arr, int n, int i) {
         int left = 2*i+1;
         int right = 2*i+2;
-        int max=i;
-        if (left <= array.length-1 && array[left] > array[i]) {
+        int max = i;
+        if(left < n && arr[left] > arr[max]) {
             max = left;
-        } else {
-            max = i;
         }
-        if(right <=array.length-1 && array[right] > array[max]) {
+        if(right < n && arr[right] > arr[max]) {
             max = right;
         }
         if(max != i) {
-            Swap.swap(array, i, max);
-            heapify(array, max);
+            Swap.swap(arr, max, i);
+            heapify(arr, n, max);
         }
     }
 }
